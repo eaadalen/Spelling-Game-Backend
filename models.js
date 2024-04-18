6e65d51d87f7b0dcb27fb7af510caa1a98461493
eaadalen <eaadalen112@gmail.com>
2024-04-18 14:35:28 -0500

    Going backwards to fix error

--- models.js
+++ models.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+let projectSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  ProjectNumber: {type: String, required: true},
+  Description: {type: String, required: true},
+  Keywords: {type: String, required: true},
+  FileLocation: {type: String, required: true},
+  ProjectManager: {type: String, required: true},
+  ProjectStaff: {type: String, required: true},
+  Systems_and_Equipment: {type: String, required: true}
+});
+
 let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
-  High_Score: {type: Number, required: true},
 });
 
 userSchema.statics.hashPassword = (password) => {
@@ -15,6 +25,8 @@ userSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
+let Project = mongoose.model('Project', projectSchema);
 let User = mongoose.model('User', userSchema);
 
+module.exports.Project = Project;
 module.exports.User = User;
